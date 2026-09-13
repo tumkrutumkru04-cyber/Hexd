4267bb5e01456c367b8604f7d6e331985aec52de
Madan Gowda <tumkrutumkru04@gmail.com>
2026-09-13 10:31:11 +0530

    Update xitexegen.php
    Signed-off-by: Madan Gowda <tumkrutumkru04@gmail.com>

--- api/xitexegen.php
+++ api/xitexegen.php
@@ -4,22 +4,20 @@ header('Access-Control-Allow-Origin: *');
 
 date_default_timezone_set('Asia/Kolkata');
 
-$redis_url = "https://alive-minnow-172519.upstash.io";
-$redis_token = "gQAAAAAAAqHnAAIgcDI0Zjk3ZTRiMWY2MTg0Mjc2YTVmYTMzNzZlY2M2OGE3OQ";
+$redis_url = "https://careful-crane-121939.upstash.io";
+$redis_token = "gQAAAAAAAdxTAAIgcDJjY2M1MWUyZWEzY2Y0YzhkYWI3ZDZmZWM4OTc3ZGMyYg";
 
-// Generate key: XITEXE-XXX-XXX
 $part1 = strtoupper(bin2hex(random_bytes(2)));
 $part2 = strtoupper(bin2hex(random_bytes(2)));
 $key = "XITEXE-" . $part1 . "-" . $part2;
 
-// 5 hours validity
 $expiry = time() + (5 * 3600);
 
 $key_data = [
     "key" => $key,
-    "device_id" => null,
+    "devices" => [],
     "devices_used" => 0,
-    "max_devices" => 1,
+    "max_devices" => 10,
     "created_at" => date('Y-m-d H:i:s'),
     "expires_at" => date('Y-m-d H:i:s', $expiry),
     "expiry_timestamp" => $expiry * 1000,
@@ -27,7 +25,6 @@ $key_data = [
     "status" => "active"
 ];
 
-// Save to Redis
 $ch = curl_init("$redis_url/set/keys:$key");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, true);
@@ -44,6 +41,6 @@ echo json_encode([
     "key" => $key,
     "validity" => "5 Hours",
     "expires_at" => date('Y-m-d H:i:s', $expiry),
-    "max_devices" => 1
+    "max_devices" => 10
 ], JSON_PRETTY_PRINT);
 ?>
